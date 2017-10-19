78387dc7a365e67c9e05f9d5affa595b6f6c9d0f
Nicolás López <nicolaslopezj@me.com>
2017-10-19 17:01:21 -0300

    create validate schema function

--- apollo-helpers.js
+++ apollo-helpers.js
@@ -2,5 +2,6 @@ import createModifier from './helpers/createModifier'
 import validate from './helpers/validate'
 import formatError from './helpers/formatError'
 import FieldError from './helpers/FieldError'
+import validateSchema from './helpers/validateSchema'
 
-export {createModifier, validate, formatError, FieldError}
+export {createModifier, validate, validateSchema, formatError, FieldError}
